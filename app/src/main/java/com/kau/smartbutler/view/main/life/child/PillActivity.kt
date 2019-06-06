bbc5815c1f94fefefc63322f0b127261d6a10367
melongl11 <melongl11@naver.com>
2019-06-06 10:49:08 +0900

    Life tab menu layout

---
+++ app/src/main/java/com/kau/smartbutler/view/main/life/child/PillActivity.kt
@@ -0,0 +1,10 @@
+package com.kau.smartbutler.view.main.life.child
+
+import com.kau.smartbutler.base.BaseActivity
+
+class PillActivity(
+        override val layoutRes: Int,
+        override val isUseDatabinding: Boolean) :
+        BaseActivity() {
+
+}
