4b60b1a5d5bc44acef24e6a7863ac51d4d6babde
melongl11 <melongl11@naver.com>
2019-06-16 18:39:44 +0900

    life tab view re-commit

---
+++ app/src/main/java/com/kau/smartbutler/view/main/life/child/ReservationHosipitalActivity.kt
@@ -0,0 +1,12 @@
+package com.kau.smartbutler.view.main.life.child
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kau.smartbutler.base.BaseActivity
+
+class ReservationHosipitalActivity(
+        override val layoutRes: Int,
+        override val isUseDatabinding: Boolean) :
+        BaseActivity() {
+
+}
