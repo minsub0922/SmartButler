9b792aef58403828576fbb3604281f7fd9deb1d6
minseop <minsub0922@naver.com>
2019-07-16 10:21:26 +0900

    [UI] CCTV Page UI

---
+++ app/src/main/java/com/kau/smartbutler/view/main/home/child/CCTVDetailActivity.kt
@@ -0,0 +1,18 @@
+package com.kau.smartbutler.view.main.home.child
+
+import com.kau.smartbutler.R
+import com.kau.smartbutler.base.BaseActivity
+
+class CCTVDetailActivity(
+        override val layoutRes : Int = R.layout.activity_device_add,
+        override val isUseDatabinding: Boolean = false)
+    : BaseActivity() {
+
+    override var isChildActivity: Boolean = true
+
+    override fun setupView() {
+        super.setupView()
+
+    }
+
+}
