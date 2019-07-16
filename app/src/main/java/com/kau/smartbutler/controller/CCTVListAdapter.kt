55851b8b1442a7c27586c5c5d583a9016e803ab5
minseop <minsub0922@naver.com>
2019-07-16 10:21:26 +0900

    [UI] CCTV Page UI

---
+++ app/src/main/java/com/kau/smartbutler/controller/CCTVListAdapter.kt
@@ -0,0 +1,49 @@
+package com.kau.smartbutler.controller
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kau.smartbutler.R
+import com.kau.smartbutler.model.CCTV
+import com.kau.smartbutler.view.main.home.child.DeviceModifiedActivity
+import java.util.*
+
+class CCTVListAdapter (
+        val mContext: Context,
+        val modelList: ArrayList<CCTV>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return DeviceListViewHolder(LayoutInflater.from(mContext).inflate(R.layout.model_device_list, parent, false))
+    }
+
+    override fun getItemCount() = modelList.size
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+
+        val model = modelList.get(position)
+
+        val holder = viewHolder as DeviceListViewHolder
+
+        holder.text.text = model.name
+
+        holder.itemView.setOnClickListener{
+
+            mContext.startActivity(Intent(mContext, DeviceModifiedActivity::class.java).putExtra("cctv", model))
+
+        }
+
+    }
+
+
+    class DeviceListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val text: TextView by lazy { itemView.findViewById<TextView>(R.id.deviceNameTextView) }
+
+    }
+}
