d36fa8c46663fccdd4253e6536c108ba0e74b6dd
alsgh <DKDML>
2019-07-20 12:38:29 +0900

    cctvDetaile cctv볼수 있도록 변경 Thread 1 개로 안정 cctv_set_deomain cctv image를 가져와서 점찎는거 까지 해결 뷰파일은 적당하게 변경

--- app/src/main/java/com/kau/smartbutler/view/main/home/child/cctv/CCTVDetailActivity.kt
+++ app/src/main/java/com/kau/smartbutler/view/main/home/child/cctv/CCTVDetailActivity.kt
@@ -1,6 +1,14 @@
 package com.kau.smartbutler.view.main.home.child.cctv
-
+//현 문제점 스레드, 해결방법 두가지 버튼(뒤로가기, 다음으로 갈때 쓰레드를 종료한다.
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import com.google.gson.JsonObject
 import com.kau.smartbutler.R
 import com.kau.smartbutler.base.BaseActivity
@@ -9,34 +17,117 @@ import com.kau.smartbutler.util.network.getCCTVNetworkInstance
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_cctv_detail.*
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import java.util.*
 
 class CCTVDetailActivity(
+
         override val layoutRes : Int = R.layout.activity_cctv_detail,
         override val isUseDatabinding: Boolean = false)
     : BaseActivity() {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+//use variable
+    internal var handler = Handler()
+    internal var OffActivity = false
+
+    internal var width = 300 // 축소시킬 너비
+    internal var height = 300 // 축소시킬 높이
+
+    internal var conf: Bitmap.Config = Bitmap.Config.ARGB_8888
+    internal var giving_image = Bitmap.createBitmap(width, height, conf)
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
     override var isChildActivity: Boolean = true
-
     lateinit var res : JsonObject
-
     override fun setupView() {
         super.setupView()
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        val iv = findViewById(R.id.cctv_image) as ImageView
+        val t = Thread(Runnable {
+            try {
+                val tt = object : TimerTask() {
+                    override fun run() {
+                        while (true) {
+                            try {
 
+                                val url = URL("http://172.16.28.2/cgi-bin/encoder?USER=admin&PWD=123456&SNAPSHOT")
+
+                                Log.d("myapp", "kkk: " + iv.width)
+                                Log.d("myapp", "kkk : "+ iv.height)
+
+                                val t_connection = url.openConnection()
+                                t_connection.readTimeout = 10000
+                                val `is` = t_connection.getInputStream()
+                                val bm = BitmapFactory.decodeStream(`is`)
+
+                                Log.d("myapp", "kkk: " + iv.width)
+                                Log.d("myapp", "kkk : "+ iv.height)
+
+                                //화면크기
+                                val display = windowManager.defaultDisplay
+                                val displayWidth = display.width
+                                val displayHeight = display.height
+
+                                val width = 700 // 축소시킬 너비
+                                val height = 525 // 축소시킬 높이
+                                var bmpWidth = bm.width.toFloat()
+                                var bmpHeight = bm.height.toFloat()
+
+                                if (bmpWidth > width) {
+                                    // 원하는 너비보다 클 경우의 설정
+                                    val mWidth = bmpWidth / 100
+                                    val scale = width / mWidth
+                                    bmpWidth *= scale / 100
+                                    bmpHeight *= scale / 100
+                                } else if (bmpHeight > height) {
+                                    // 원하는 높이보다 클 경우의 설정
+                                    val mHeight = bmpHeight / 100
+                                    val scale = height / mHeight
+                                    bmpWidth *= scale / 100
+                                    bmpHeight *= scale / 100
+                                }
+
+
+                                val resized = Bitmap.createScaledBitmap(bm, cctv_image.right, cctv_image.bottom, true)
+                                val resized2 = Bitmap.createScaledBitmap(bm, width, height, true)
+
+                                giving_image = resized2
+
+                                handler.post {
+                                    // 화면에 그려줄 작업
+                                    //iv.setImageBitmap(resized);
+                                    iv.setImageBitmap(resized)
+                                }
+
+                                iv.setImageBitmap(resized) //비트맵 객체로 보여주기
+                                iv.invalidate()
+                            } catch (e: Exception) {
+                                Log.d("myapp", "Error : $e")
+                            }
+                        }
+                    }
+                }
+                val timer = Timer()
+                timer.schedule(tt, 0, 500)
+            } catch (e: Exception) {
+            }
+        })
+        t.start()
         moreNavButton.setOnClickListener {
-            startActivity(Intent(this, CCTVSetDomainActivity::class.java))
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+//버튼 클릭 리스너
+            val bStream = ByteArrayOutputStream()
+            giving_image.compress(Bitmap.CompressFormat.PNG, 0, bStream)
+            val byteArray = bStream.toByteArray()
+            val i = Intent(this, CCTVSetDomainActivity::class.java)
+            i.putExtra("image", byteArray)
+            OffActivity = true
+            finish()
+            startActivity(i)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
         }
-
-
-        //네트워크 Examples
-        getCCTVNetworkInstance()
-                .postDetectionArea(PostDetectionAreaRequest("","","",""))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ res ->
-
-                    this.res = res
-
-                })
     }
 
 }
--- app/src/main/java/com/kau/smartbutler/view/main/home/child/cctv/CCTVSetDomainActivity.kt
+++ app/src/main/java/com/kau/smartbutler/view/main/home/child/cctv/CCTVSetDomainActivity.kt
@@ -1,22 +1,219 @@
 package com.kau.smartbutler.view.main.home.child.cctv
-
+//현문제. 화면크기를 imageview에 맞추면 된다리. 버튼 누를때 ontouch와 imageview의 톱 위치를 맞게 설정
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.*
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
 import com.kau.smartbutler.R
 import com.kau.smartbutler.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_cctv_set_domain.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class CCTVSetDomainActivity(
         override val layoutRes : Int = R.layout.activity_cctv_set_domain,
         override val isUseDatabinding: Boolean = false)
     : BaseActivity() {
+    var mPaint = Paint()
+    //터치 한 곳의 좌표를 저장할 변수
+    var x = -1f
+    var y = -1f
+    //터치했던곳의 좌표 배열
+    internal var vy = ArrayList<Float>()
+    internal var vx = ArrayList<Float>()
+
+    //상대좌표
+    internal var relative_coordinate_X = -1f
+    internal var relative_coordinate_Y = -1f
+
+    //상대좌표 list
+    internal var relative_coordinateX_V = ArrayList<Float>()
+    internal var relative_coordinateY_V = ArrayList<Float>()
+
+    //좌표를 ArrayList에 저장할지 설정 ( 1 받음 0 안받음)
+    internal var input_flag = 1
+    // 이전단계의 터치 좌표
+    private var start_x = -1f
+    private var start_y = -1f
+    //경로 설정 변수(이거대신 일단 drawLine씀)
+    private val path: Path? = null
+    /////////////////////////////////////////////////////////////////////////////
+    internal var sObject = JSONObject()
+
+    internal var coordinates = java.util.ArrayList<java.util.ArrayList<*>>()
+    internal var coordinates_full = java.util.ArrayList<java.util.ArrayList<*>>()
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+//onDrawfunction
+    internal fun onDraw(canvas :Canvas) {
+        mPaint.setColor(Color.GREEN)
+        mPaint.setStrokeWidth(5F)
+        mPaint.setStyle(Paint.Style.STROKE)
+        mPaint.setAntiAlias(true)
+        mPaint.setDither(true)
+
+        if (x > 0 && y > 0) {
+            start_x = x
+            start_y = y
+            for (i in 0 until vx.size) {
+                if (i == 0) {
+                    canvas.drawCircle(vx[0], vy[0], 5f, mPaint)
+                }
+                if (i > 0) {
+                    start_x = vx[i - 1]
+                    start_y = vy[i - 1]
+                    x = vx[i]
+                    y = vy[i]
+                    if (Math.abs(vx[0] - x) < 100 && Math.abs(vy[0] - y) < 100) {
+                        input_flag = 0//더이상 입력을 받지 않음
+                        canvas.drawLine(start_x, start_y, vx[0], vy[0], mPaint)
+                        break
+                    }
+                    canvas.drawCircle(vx[i], vy[i], 10f, mPaint)
+                    canvas.drawLine(start_x, start_y, x, y, mPaint)
+                }
+            }
+            start_x = x
+            start_y = y
+        }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    fun initJson() {
+        val arr = JSONArray()
+
+        try {
+            sObject.put("Intrusion", arr)
+            sObject.put("Loitering", arr)
+            sObject.put("Abandon", arr)
+            sObject.put("Falldown", arr)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun set_detect_area_event(event_name: String, is_reset: Boolean) {
+        try {
+            sObject.remove(event_name)
+            if (is_reset)
+                sObject.put(event_name, "[]")
+            else if (event_name == "Falldown") {
+                sObject.put(event_name, coordinates_full)
+            } else {
+                sObject.put(event_name, coordinates)
+            }
+            Log.d("[INFO] coordinates : ", coordinates.toString())
+        } catch (e: JSONException) {
+            Log.d("[INFO] ERROR : ", e.toString())
+        }
+    }
+
+
+
+
 
     override var isChildActivity: Boolean = true
-
+    @SuppressLint("ClickableViewAccessibility")
     override fun setupView() {
         super.setupView()
+        val iv = findViewById(R.id.cctv_image) as ImageView
+        val arr = intent.getByteArrayExtra("image")
+        val bm = BitmapFactory.decodeByteArray(arr, 0, arr.size)
+        val resized = Bitmap.createScaledBitmap(bm,1080 , 1170, true)
+        var canvas =Canvas()
+        iv.setImageBitmap(resized)
 
+
+        initJson()
+
+//패인트 색깔
+        mPaint.setColor(Color.BLUE)
+        mPaint.setStrokeWidth(10F)
+        mPaint.setStyle(Paint.Style.STROKE)
+        mPaint.setAntiAlias(true)
+        mPaint.setDither(true)
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//will use variable
+        canvas.setBitmap(resized)
+        Log.d("myapp", "AAA2: " + iv.width)
+        Log.d("myapp", "AAA2: "+ iv.height)
+
+        iv.setOnTouchListener(){ view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("myapp", "AAA3: " + iv.width)
+                    Log.d("myapp", "AAA3: "+ iv.height)
+                    x = motionEvent.getX()
+                    y = motionEvent.getY()
+                    relative_coordinate_X = x/1080f
+                    relative_coordinate_Y = y/1170f
+                    //좌표벡터에 넣어준다.
+                    if (input_flag == 1) {
+                        vx.add(x.toFloat())
+                        vy.add(y.toFloat())
+                        relative_coordinateX_V.add(relative_coordinate_X)
+                        relative_coordinateY_V.add(relative_coordinate_Y)
+                        Log.d("myapp", "좌표X: " + x)
+                        Log.d("myapp", "좌표Y : "+ y)
+
+                        Log.d("setdo", "RCX: " + relative_coordinate_X)
+                        Log.d("sedo", "RCY : "+ relative_coordinate_Y)
+
+                    }
+                }
+            }
+
+            onDraw(canvas)
+            iv.invalidate()
+            return@setOnTouchListener true
+        }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        //button event
         stateButton1.setOnClickListener {
+            if (stateButton1.isChecked()) {
+                Log.d("Intrusion", "Enable")
+                set_detect_area_event("Intrusion", false)
+            } else {
+                Log.d("Intrusion", "Disable")
+                set_detect_area_event("Intrusion", true)
+            }
+        }
 
+        stateButton2.setOnClickListener {
+            if (stateButton2.isChecked()) {
+                Log.d("Loitering", "Enable")
+                set_detect_area_event("Loitering", false)
+            } else {
+                Log.d("Loitering", "Disable")
+                set_detect_area_event("Loitering", true)
+            }
+        }
+
+        stateButton3.setOnClickListener {
+            if (stateButton3.isChecked()) {
+                Log.d("Abandon", "Enable")
+                set_detect_area_event("Abandon", false)
+            } else {
+                Log.d("Abandon", "Disable")
+                set_detect_area_event("Abandon", true)
+            }
+        }
+
+        stateButton4.setOnClickListener {
+            if (stateButton4.isChecked()) {
+                Log.d("Falldown", "Enable")
+                set_detect_area_event("Falldown", false)
+            } else {
+                Log.d("Falldown", "Disable")
+                set_detect_area_event("Falldown", true)
+            }
         }
     }
 }
